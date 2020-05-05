d82ac803a2ece47a3bd924423ed1f63b7d3ebcb4
Jules Casteran <jules.casteran@gmail.com>
2020-05-05 18:52:57 +0200

    Send query to leon added Socket io dependency added permission to android app and use cleartexttraffic in application Use socket in ChatActivity

--- leonApp/app/src/main/java/com/poc/leonapp/ChatActivity.kt
+++ leonApp/app/src/main/java/com/poc/leonapp/ChatActivity.kt
@@ -1,22 +1,21 @@
 package com.poc.leonapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
 import com.poc.leonapp.models.ChatLeon
 import com.poc.leonapp.models.ChatUser
 import com.poc.leonapp.models.User
 import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.parcel.Parcelize
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_chat.*
-import kotlinx.android.synthetic.main.chat_leon.view.*
-import kotlinx.android.synthetic.main.chat_user.view.*
+import org.json.JSONObject
 
 class ChatActivity : AppCompatActivity() {
     private val adapter = GroupAdapter<ViewHolder>()
+    var socket: Socket = IO.socket("http://192.168.1.30:1337")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,26 +24,44 @@ class ChatActivity : AppCompatActivity() {
         userSend.setOnClickListener {
             performSendUserRequest()
         }
+
+        socket.on(Socket.EVENT_DISCONNECT, Emitter.Listener {
+            println("disconnected")
+        })
+        socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
+            println("connected")
+            socket.emit("init", "webapp")
+        })
+        socket.on("answer", Emitter.Listener {
+            addLeonResponseToChat("response")
+        })
+        socket.connect()
+        println("SOCKET CONNECT")
     }
 
     private fun performSendUserRequest() {
         // TODO : Send user request to Leon
         addUserRequestToChat()
-        addLeonResponseToChat()
+        //addLeonResponseToChat("okok")
     }
 
-    private fun addLeonResponseToChat() {
-        val leon = User("Leon says hello !")
+    private fun addLeonResponseToChat(response: String) {
+        val leon = User(response)
 
         adapter.add(ChatLeon(leon))
         chatRecyclerView.scrollToPosition(adapter.itemCount - 1)
     }
 
     private fun addUserRequestToChat() {
-        val user = User(userKeyboard.text.toString())
+        val msg: String = userKeyboard.text.toString()
+        val user = User(msg)
 
         adapter.add(ChatUser(user))
         userKeyboard.text.clear()
         chatRecyclerView.scrollToPosition(adapter.itemCount - 1)
+        val obj = JSONObject();
+        obj.put("value", msg)
+        socket.emit("query", obj)
     }
+
 }
